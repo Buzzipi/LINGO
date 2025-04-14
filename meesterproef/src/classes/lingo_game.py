011599bf341fc524e7f4cd427293df4f27876de5
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-04-14 19:11:54 +0200

    finished word guessing logic [meesterproef]
    The logic where a user guesses a word is finished!
    
    I ran into some issues when i was comparing the user guessed word with the random word. Like for example in the word woo, both o's being yellow marked, while the random word, wow, only has 1 o.
    
    This would be really confusing for the user, so i made sure that the letters in the user guessed word, were only yellow market if they didn't exceed the number of occurences in the random word.
    
    I did this with the help of 2 dictionaries, one for the amount of reacurring letters in the random word (with the help of the counter attribute, and dict, to make sure the layout of the word was still correct)
    
    And a dict for the user guessed word letters, each letter has a 0 assigned too it, and every time the letter loop is initialized, the selected letter gets added by 1
    
    Then i added an if statement with a try catch too see if the letter count in the guessed word dict exceeded the letter count in the random word dict, if so, a continue is executed.
    
    The reason this if is in a try catch is because it is not always certain set letter is contained in the random word dict, hense why if a keyError accurs, a continue attribute is executed which restarts the loop.

--- meesterproef/src/classes/lingo_game.py
+++ meesterproef/src/classes/lingo_game.py
@@ -1,5 +1,6 @@
 from src.data.lingowords import *
 import random
+from collections import Counter
 
 class lingo_game:
     def __init__(self, team1, team2):
@@ -14,29 +15,83 @@ class lingo_game:
         print()
         print("LETS BEGIN!")
         random_word = list(self.random_selected_word(words))
+        random_word_duplicate_letters = dict(Counter(random_word))
         
         word_lines = []
         
-        for _ in range(len(random_word)):
-            word_lines.append("_")
-            
+        word_lines.append(random_word[0])
+        
+        copied_list = random_word.copy()
+        amount_of_letters_in_word = copied_list[1:] # skips the 0 index and only returns all the values after index 0
+        
+
+        for letter in amount_of_letters_in_word:
+            print(letter)
+            if letter != '' or ' ':
+                word_lines.append("_")
+            else:
+                word_lines.append("")
+        
+        turn = 0
+        
+        break_loop = False
+        
         while True:
-            print(random_word)
-            for i in range(5):
-                print(word_lines) 
+            
+            if break_loop == True:
+                break
+            
+            if turn == 5:
+                print(random_word)
+                break
+                
+            for _ in range(5):
+                turn += 1
+                print(turn)
+                guessed_word = '  '.join(word_lines)
+                print(guessed_word)
                 user_guess = list(input("guess the word:  "))
+                user_guess_duplicate_keys = {word: 0 for word in user_guess}
                 
                 index = 0
-                for letter in user_guess:
-                    if(letter in random_word):
-                        word_lines[index] = letter
-                        
-                if(user_guess in random_word):
-                    for i in range(len(random_word)):
-                        if random_word[i] == user_guess:
-                            word_lines[i] == user_guess
-                            break
                 
+                if len(user_guess) > len(random_word):
+                    print("this word is too long!")
+                    continue
+                if len(user_guess) < len(random_word):
+                    print("this word is too short!")
+                    continue
+                
+                
+                for letter in user_guess:
+                    
+                    word_lines[index] = letter
+                    user_guess_duplicate_keys[letter] += 1
+                    
+                    if(letter not in random_word):
+                        word_lines[index] = letter
+                    if(letter in random_word):    
+                        word_lines[index] = f"\033[93m{letter}\033[0m"
+                    if letter == random_word[index]:
+                        if letter in word_lines:
+                            continue
+                        else: 
+                            word_lines[index] = f"\033[92m{letter}\033[0m"
+                    try:
+                        if user_guess_duplicate_keys[letter] > random_word_duplicate_letters[letter]:
+                            word_lines[index] = letter
+                    except KeyError:
+                        continue
+                        
+                        
+                    index += 1
+                
+                print(user_guess_duplicate_keys)
+                
+                if user_guess == random_word:
+                    print("you guessed the word!")
+                    break_loop = True
+                    break
                 
             
     
