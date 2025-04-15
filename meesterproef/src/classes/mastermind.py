0c6b808795028fff711e30706c9e94089406a628
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-04-15 18:30:07 +0200

    new mastermind class [meesterproef]
    decided too put the word guessing logic in a seperate class, this helps me keep things organized and easier to maintain as the project grows. And in the mastermind class, i also splitted up different logics like selecting a random word into methods.
    
    this helps the class become easier to read and maintain :)
    
    with the mastermind_game method in the mastermind class, a random word is selected, the duplicate letter list is made to keep track of duplicate letters. The visual word guesser is set up with the new prepare_word() method, and the word_guessing_logic() is executed

---
+++ meesterproef/src/classes/mastermind.py
@@ -0,0 +1,98 @@
+import random
+from collections import Counter
+from src.data.lingowords import *
+
+class Mastermind:
+    def random_selected_word(self, words_list):
+        randomly_selected_word = random.choice(words_list)
+        return randomly_selected_word
+    
+    def prepare_word(self, word_lines, random_word):
+        word_lines.append(random_word[0])
+        
+        copied_list = random_word.copy()
+        amount_of_letters_in_word = copied_list[1:] # skips the 0 index and only returns all the values after index 0
+        
+
+        for letter in amount_of_letters_in_word:
+            print(letter)
+            if letter != '' or ' ':
+                word_lines.append("_")
+            else:
+                word_lines.append("")
+    
+    def word_guessing_logic(self, random_word, word_lines, random_word_duplicate_letters, team):
+        turn = 0
+        
+        break_loop = False
+        
+        while True:
+            
+            if break_loop == True:
+                break
+            
+            if turn == 5:
+                print(random_word)
+                break
+                
+            for _ in range(5):
+                turn += 1
+                print(turn)
+                guessed_word = '  '.join(word_lines)
+                print(guessed_word)
+                user_guess = list(input(f"{team.team_name}'s turn too guess the word:  "))
+                user_guess_duplicate_keys = {word: 0 for word in user_guess}
+                
+                index = 0
+                
+                if len(user_guess) > len(random_word):
+                    print("this word is too long!")
+                    continue
+                if len(user_guess) < len(random_word):
+                    print("this word is too short!")
+                    continue
+                
+                
+                for letter in user_guess:
+                    
+                    word_lines[index] = letter
+                    user_guess_duplicate_keys[letter] += 1
+                    
+                    if(letter not in random_word):
+                        word_lines[index] = letter
+                    if(letter in random_word):    
+                        word_lines[index] = f"\033[93m{letter}\033[0m"
+                    if letter == random_word[index]:
+                        if letter in word_lines:
+                            continue
+                        else: 
+                            word_lines[index] = f"\033[92m{letter}\033[0m"
+                    try:
+                        if user_guess_duplicate_keys[letter] > random_word_duplicate_letters[letter]:
+                            word_lines[index] = letter
+                    except KeyError:
+                        continue
+                        
+                        
+                    index += 1
+                
+                print(user_guess_duplicate_keys)
+                
+                if user_guess == random_word:
+                    print(f"{team.team_name} guessed the word!")
+                    break_loop = True
+                    team.amountOfGuessedWords += 1
+                    break
+        
+        
+     
+    def mastermind_game(self, team):
+        random_word = list(self.random_selected_word(words))
+        random_word_duplicate_letters = dict(Counter(random_word))
+        
+        
+        word_lines = []
+        
+        self.prepare_word(word_lines, random_word)
+    
+        self.word_guessing_logic(random_word, word_lines, random_word_duplicate_letters, team)
