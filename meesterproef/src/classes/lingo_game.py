c63945abb865df361fda13de82da42a69c5467c8
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-04-16 17:15:39 +0200

    applying the new basket feature too the lingo_game class [meesterproef]
    Too ensure a team can select a random ball from their basket after they have correctly guessed a word, i first needed to create a basket instance (line 16). Here i take the 2 teams and create their baskets. Team 1 has 3 green balls, 3 red balls and 17 odd numbers, Team 2 has the same, with the only difference being that they have even numbers instead of odd.
    
    after that, i make sure that the baskets are created when the start_game() method is executed. Then i have 2 variables in the main loop that keep track of the current number of guessed words, this is needed for the following 2 new if's
    
    these new if's (line 59 and 49) will keep track of any changes too the amountOfGuessedWords() field, if the if notices that the amountOfGuessedWords has increased, the randomBallfromBasket() method is executed

--- meesterproef/src/classes/lingo_game.py
+++ meesterproef/src/classes/lingo_game.py
@@ -2,6 +2,7 @@ from src.data.lingowords import *
 from src.classes.mastermind import *
 import random
 from collections import Counter
+from src.classes.basket import *
 
 class lingo_game:
     def __init__(self, team1, team2):
@@ -12,10 +13,23 @@ class lingo_game:
         MM = Mastermind()
         MM.mastermind_game(team)
     
+    def CreatingBaskets(self, team1, team2):
+        basket = Basket()
+        basket.CreateBaskets(team1, team2)
+        
+    
     def start_game(self):
         print()
         print("LETS BEGIN!")
+        
+        self.CreatingBaskets(self.team1, self.team2)
+        
         while True:
+            
+            
+            current_score_team_1 = self.team1.amountOfGuessedWords
+            current_score_team_2 = self.team2.amountOfGuessedWords
+            
             
             if (self.team1.amountOfFailedGuessedWords >= 3):
                 print(f"🎉{self.team2.team_name} has won the game, because {self.team1.team_name} failed to guess a word 3 times in a row!!🎉")
@@ -25,15 +39,33 @@ class lingo_game:
                 print(f"🎉{self.team1.team_name} has won the game, because {self.team2.team_name} failed to guess a word 3 times in a row!!🎉")
                 break
             
+            
+            # team 1's turn
             self.Mastermind(self.team1)
             if (self.team1.amountOfGuessedWords >= 10):
                 print(f"🎉{self.team1.team_name} guessed 10 words correctly!!, they are the lingo winners!🎉")
                 break
             
+            if (self.team1.amountOfGuessedWords > current_score_team_1):
+                self.team1.RandomBallFromBasket()
+            
+            
+            # team 2's turn
             self.Mastermind(self.team2)
             if (self.team2.amountOfGuessedWords >= 10):
                 print(f"🎉{self.team2.team_name} guessed 10 words correctly!!, they are the lingo winners!🎉")
                 break
+            
+            if (self.team2.amountOfGuessedWords > current_score_team_2):
+                self.team2.RandomBallFromBasket()
+        
+        print()
+        print()
+        print(f"{self.team1.team_name} ball possession: {self.team1.ballsInPossession}")
+        print(f"{self.team2.team_name} ball possession: {self.team2.ballsInPossession}")
+        print()
+        print()
+                
        
         
                 
