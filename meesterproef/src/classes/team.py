f6d7c4d9de5e5680d6fa8fe344f85ca7bf7a640a
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-04-16 17:19:05 +0200

    new fields and methods Team class [meesterproef]
    I've created 2 new fields and 1 new method for the team class.
    
    New fields:
    
    - basket:
    This will save the basket of the team
    
    - ballsInPossession:
    This fields keeps track of all the balls a team has in possession
    
    New methods:
    
    - RandomBallFromBasket():
    this new method selects a random ball from the basket field, prints what ball the team has gotten, and appends it too the ballsInPossession field.

--- meesterproef/src/classes/team.py
+++ meesterproef/src/classes/team.py
@@ -1,3 +1,5 @@
+import random
+
 class Team:
     def __init__(self, team_name, member1, member2):
         self.team_name = team_name
@@ -5,6 +7,8 @@ class Team:
         self.member2 = member2
         self.amountOfGuessedWords = 0
         self.amountOfFailedGuessedWords = 0
+        self.basket = []
+        self.ballsInPossession = []
         
     def ShowTeamMembers(self):
         print(f"first team member: {self.member1.name}")
@@ -12,3 +16,10 @@ class Team:
         
     def ShowTeamName(self):
         print(f"team name: {self.team_name}")
+        
+    def RandomBallFromBasket(self):
+        random_ball = random.choice(self.basket)
+        
+        print(f"you got a {random_ball} ball!")
+        
+        self.ballsInPossession.append(random_ball)
