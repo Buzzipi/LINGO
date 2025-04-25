92a7450419e1505b503b978b15a75171f22eee72
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-04-25 13:51:07 +0200

    animation class changes [meesterproef]
    [intro class]:
    
    - decided too edit the intro animation to make it look a bit cleaner
    
    [main menu class]:
    
    - i decided too add a main menu animation for easier readability for the user, the stats option will be added later on

--- meesterproef/src/classes/animations.py
+++ meesterproef/src/classes/animations.py
@@ -1,21 +1,26 @@
 from src.data.art import *
 import time
+import os
 
 
 class animations:
     @staticmethod
     def intro():
+        os.system('cls')
+        
+        time.sleep(2)
+        print(lingo_title)
+        time.sleep(2)
+        
         for line in text_banner_lingo:
             time.sleep(0.2)
             print(line)
     
         print() 
-
-        time.sleep(2)
-        print(lingo_title)
-        time.sleep(2)
         
         print()
+        time.sleep(2)
+        os.system('cls')
     
     @staticmethod
     def outro():
@@ -24,4 +29,16 @@ class animations:
         for line in outro_banner:
             time.sleep(0.2)
             print(line)
+    
+    @staticmethod
+    def main_menu():
+        print()
+        print()
+        print("----[:MAIN MENU:]----")
+        print("- play:  (type: P)")
+        print("- quit:  (type: Q)")
+        print("- stats: (type: S)")
+        print()
+        print()
+        
         
