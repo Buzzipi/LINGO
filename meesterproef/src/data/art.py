14e45012c51e242cec06fdd369d98e55e2ca843d
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-04-29 13:22:26 +0200

    Python auto-caching

--- meesterproef/src/data/art.py
+++ meesterproef/src/data/art.py
@@ -1,6 +1,6 @@
 from colorama import *
 
-lingo_title = "W E L C O M E     T O     \033[91m【L】\033[0m\033[93m【i】\033[0m\033[94m【n】\033[0m\033[92m【g】\033[0m\033[95m【o】\033[0m💭!"
+lingo_title = "W E L C O M E     T O     . . ."
 
 text_banner_lingo =[" ___      ___   __    _  _______  _______",
                     "|   |    |   | |  |  | ||       ||       |",
